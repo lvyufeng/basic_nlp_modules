e9f68f9df96f7ced8119172738aed57090a7bde9
lvyufeng <lvyufeng@cqu.edu.cn>
2020-04-28 22:47:05 +0800

    add scaled dot product attention

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Implementation of some basic nlp modules, like RNN, LSTMs, Transformer, etc..
 
 简单实现以下一些基本的NLP组件，一来加深记忆，二来免得面试被问住。。。
 
+- [ ] MLP
 - [x] LSTM
 - [ ] GRU
 - [ ] Normal Attention
 - [ ] Sequence2Sequence
 - [ ] Self-Attention
+- [x] Self-Attention(Scaled Dot)
 - [ ] Multi-Head Attention
 - [ ] Transformer
---
+++ scaled_dot_product_attention.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import math
+
+class ScaledDotAttention(nn.Module):
+    """
+    Attention(Q,K,V) = softmax(Q*K^T/sqrt(d_k))*V
+    """
+    def __init__(self, key_size, value_size, dropout = 0.0):
+        super(ScaledDotAttention,self).__init__()
+        self.key_size = key_size
+        self.value_size = value_size
+        self.scale = math.sqrt(key_size)
+        self.dropout = nn.Dropout(dropout)
+        self.softmax = nn.Softmax(dim = -1)
+
+    def forward(self, Q, K, V):
+        
+        output = torch.matmul(Q,K.transpose(-1,-2)) / self.scale
+        output = self.softmax(output)
+        output = self.dropout(output)
+        return torch.matmul(output,V)
